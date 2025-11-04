44872fd9d61e1f98b0aabea403d5da38d1dcd896
cleybson7 <cleybsonsouza5@gmail.com>
2025-11-04 20:47:33 -0300

    FIX: corrigir a SpecificationTemplate

--- authuser/src/main/java/com/ead/authuser/controllers/UserController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.ead.authuser.controllers;
 import com.ead.authuser.dtos.UserDto;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.service.UserService;
+import com.ead.authuser.specification.SpecificationTemplate;
 import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.BeanUtils;
 import org.springframework.data.domain.Page;
@@ -30,8 +31,9 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping
-    public ResponseEntity<Page<UserModel>> getAllUsers(@PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC)
+    @GetMapping//minuto 17
+    public ResponseEntity<Page<UserModel>> getAllUsers(
+            @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC)
                                                        Pageable pageable){
         Page<UserModel> userModelPage = userService.findAll(pageable);
         return ResponseEntity.status(HttpStatus.OK).body(userModelPage);
